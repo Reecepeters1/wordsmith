6b2daa5a754838d28008fe7ac870b4d33f50b4cb
JOHN KRUEGER <802781@ehs-s-232-07.local>
2017-12-07 11:43:06 -0600

    Started Card class

---
+++ wordsmith/Card.swift
@@ -0,0 +1,18 @@
+//
+//  Card.swift
+//  wordsmith
+//
+//  Created by KRUEGER, JOHN on 12/7/17.
+//  Copyright © 2017 District196. All rights reserved.
+//
+
+import Foundation
+
+class Card {
+    
+    var responses:[Card]?
+    //TODO: Freely drawn image
+    let speech:Int?
+    let positionInSpeech:Int?
+    
+}
