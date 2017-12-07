9ce1a6877494b7e19dca319a4341f08ef31c57e2
JOHN KRUEGER <802781@ehs-s-232-07.local>
2017-12-07 12:22:40 -0600

    Created CardHolder, and Flow

---
+++ wordsmith/Flow.swift
@@ -0,0 +1,16 @@
+//
+//  Flow.swift
+//  wordsmith
+//
+//  Created by KRUEGER, JOHN on 12/7/17.
+//  Copyright © 2017 District196. All rights reserved.
+//
+
+import Foundation
+
+class Flow {
+    
+    var position:[[CardHolder]]?
+    
+    
+}
