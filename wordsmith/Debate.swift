be5a5d2c321125c44e9413ad1f9502a1453f883e
JOHN KRUEGER <802781@ehs-s-232-07.local>
2017-12-11 11:23:36 -0600

    Renamed Flow property position to cards for clarity

---
+++ wordsmith/Debate.swift
@@ -0,0 +1,28 @@
+//
+//  Debate.swift
+//  wordsmith
+//
+//  Created by KRUEGER, JOHN on 12/8/17.
+//  Copyright © 2017 District196. All rights reserved.
+//
+
+import Foundation
+
+class Debate {
+    
+    var positions:[Flow]?
+    var title:String?
+    var roundNumber:Int?
+    var otherTeam:String?
+    var expirationDate:Date?
+    var winLoss:Bool?
+    var judgeName:String?
+    var dateCreated:Date?
+    
+    
+    //generic init
+    init() {
+        
+    }
+    
+}
