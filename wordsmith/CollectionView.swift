c6b7e7583498f897a828e289a34c3cca90cd01ab
SHIH, FREDERIC <762947@ehs-s-233-02.district196.org>
2017-12-11 12:26:49 -0600

    deleted story board

---
+++ wordsmith/CollectionView.swift
@@ -0,0 +1,21 @@
+//
+//  CollectionView.swift
+//  wordsmith
+//
+//  Created by SHIH, FREDERIC on 12/11/17.
+//  Copyright © 2017 District196. All rights reserved.
+//
+
+import UIKit
+
+class CollectionView: UICollectionView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
