8a7d35bc1e9cf72fb30e3037049274db14f1eac0
802781 <802781@apps.district196.org>
2018-01-26 11:19:15 -0600

    Added comments

--- wordsmith/Storage.swift
+++ wordsmith/Storage.swift
@@ -12,13 +12,16 @@ import os.log
 
 public class Storage: NSObject, NSCoding {
     
+    //This is the array of Debate objects that gets recalled, then pased to main menue.
+    //Also the array that the main menu copies unto when it saves.
     static var myArray:[Debate]?
     
-    
+    //Static structure that lets us restore our data
     struct PropertyKey {
         static let name = "name"
     }
     
+    //This is the location where the files are archived
     static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
     static let ArchiveUrl = DocumentsDirectory.appendingPathComponent("wordsmith")
     
@@ -27,6 +30,7 @@ public class Storage: NSObject, NSCoding {
         Storage.myArray = array
     }
     
+    //takes a deconder, and uses magic to retrieve our data from the file system.
     required convenience public init?(coder aDecoder: NSCoder) {
         guard let ThisArray = aDecoder.decodeObject(forKey: PropertyKey.name) as? [Debate] else {
             os_log("Unable t Decode the name for a myArrayObject.", log: OSLog.default, type: .debug)
@@ -36,6 +40,7 @@ public class Storage: NSObject, NSCoding {
 
     }
     
+    //uses magic to make an archive of data. I don't undertand how this work, but it does
     public func encode(with aCoder: NSCoder) {
         aCoder.encode(Storage.myArray!, forKey: PropertyKey.name)
     }
