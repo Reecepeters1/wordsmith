d44547b6419b1d504aa0a3617b99f9b059c13b63
freddie <freddie@ehs-s-xlib-110.local>
2018-04-23 07:54:24 -0500

    4/23 freddy still need to fix the collection view but things work a lot better now

---
+++ wordsmith/FlowViewDataSource.swift
@@ -0,0 +1,15 @@
+//
+//  FlowViewDataSource.swift
+//  wordsmith
+//
+//  Created by freddie on 4/22/18.
+//  Copyright © 2018 District196. All rights reserved.
+//
+
+import UIKit
+
+class FlowViewDataSource: UICollectionView {
+
+    
+
+}
